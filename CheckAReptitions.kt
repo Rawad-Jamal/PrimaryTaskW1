2450b378286f96d2cd260ed9997e660d229615ba
GitHubRawadJamal <106017266+GitHubRawadJamal@users.noreply.github.com>
2022-05-22 15:14:09 +0300

    Update CheckAReptitions.kt

--- CheckAReptitions.kt
+++ CheckAReptitions.kt
@@ -1,25 +1,18 @@
 fun checkListChars(abList:List<String>):Int{
     var aCount=0
     var returnValue=0
-    var aPercentage=0.0
     loop1@ for(element in abList){
         if(element=="a" || element=="b" ){
             if(element=="a") aCount++ else {}
-            //returnValue=aCount
+            returnValue=aCount
         }
         else { returnValue=-1 ; break@loop1 }
     }
-    if(returnValue!=-1){
-        aPercentage=(aCount*100)/abList.size.toDouble()
-        println("The Percentage of char 'a' Repetitions in abCharList: "+"%.1f".format(aPercentage))
-    }
-
     return returnValue
 }
 
 fun main() {
     val abCharList=listOf("a","b","b")
-
-    //var aPercentage=(checkListChars(abCharList)*100)/abCharList.size.toDouble()
-   // println("The Percentage of char 'a' Repetitions in abCharList: "+"%.1f".format(aPercentage))
+    var aPercentage=(checkListChars(abCharList)*100)/abCharList.size.toDouble()
+    println("The Percentage of char 'a' Repetitions in abCharList: "+"%.1f".format(aPercentage))
 }
